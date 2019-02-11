cc2ff4d29b1f7c75a6aa2bc4aa76ab5bd27b80dd
Gaillard Clara <gaillardc@b13p8.fil.univ-lille1.fr>
2019-02-11 12:04:50 +0100

    First part of the TP (1 to 6)

---
+++ src/main/java/com/miage/altea/tp/pokemon_ui/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.miage.altea.tp.pokemon_ui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @PostMapping("/registerTrainer")
+    public ModelAndView registerNewTrainer(String trainerName){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("name", trainerName);
+        modelAndView.setViewName("register");
+        return modelAndView;
+    }
+}
